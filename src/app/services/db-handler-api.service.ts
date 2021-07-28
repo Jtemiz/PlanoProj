693c59a847a83e3007347f4b7cf98a97eb2eba68
Joel Temiz <joel.temiz@unibw.de>
2021-07-28 20:54:02 +0200

    adding documentation

--- src/app/services/db-handler-api.service.ts
+++ src/app/services/db-handler-api.service.ts
@@ -13,11 +13,19 @@ export class DBHandlerApiService {
   public messwert: Messwert[];
   private apiURL = 'http://192.168.178.153:5000/';
 
+  /**
+   * Sends a request to the API to set an comment on an specific position of the measurement.
+   * Put-Request accessable on 192.168.178.153/ with body [kommentar: string, position: int]
+   */
   setComment(str: string, at: number) {
    return this.http.put<any>(this.apiURL, {kommentar: str, position: at})
       .subscribe(data => console.log(data));
   }
 
+  /**
+   * Sends a request to the API to Create a new Table with the current Date as name (Format YYYYMMDDHHMM).
+   * Put-Request accessable on 192.168.178.153/start with body tablename: int
+   */
   createTable() {
     const date: Date = new Date(Date.now());
     const tabN: string = date.getFullYear()
@@ -26,17 +34,30 @@ export class DBHandlerApiService {
       + ('0' + date.getHours()).slice(-2)
       + ('0' + date.getMinutes()).slice(-2);
     console.log('tableName generated ' + tabN)
-    this.http.put<any>(this.apiURL + 'management', {tableName: tabN} ).subscribe(data => console.log("created new table: " + tabN + "; Return Value = " + data),
+    this.http.put<any>(this.apiURL + 'start', {tableName: tabN} ).subscribe(data => console.log("created new table: " + tabN + "; Return Value = " + data),
       error => console.log(error));
   }
 
+  /**
+   * Sends a request on API to stop the measuring on the Arduino.
+   * Get-Request is accessable on 192.168.178.153/stop
+   */
   stopArduino() {
     this.http.get(this.apiURL + 'stop');
   }
+
+  /**
+   * sends a request to the API to get all values that haven't been received yet.
+   * Get-request is accessable on 192.168.178.153/<Param: int>
+   * @param lastPos means the newest received value
+   */
   getNewValues(lastPos: number) {
     return this.http.get<Messwert[]>(this.apiURL + lastPos);
   }
-
+  /**
+   * sends a request to the API to get all existing tables in the Database (so all done measurements)
+   * Get-request is accessable on 192.168.178.153/management
+   */
   getAllTables() {
     return this.http.get<string[]>(this.apiURL + 'management');
   }
